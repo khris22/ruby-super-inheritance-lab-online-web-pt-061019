47d01fc9608b11658792c3ed163602d775b05883
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-09 20:34:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,8 @@
 class Student
 
   def hello
-    greeting = "Hey there! I'm so excited to learn stuff."
-    greeting
+    @greeting = "Hey there! I'm so excited to learn stuff."
+    @greeting
   end
 
   def raise_hand
