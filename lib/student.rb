cb625d35419b204200e220d32a84cae17d6f7532
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-09 20:39:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
 class Student
   attr_accessor :name
 
-  def initialize
-    @name = name
-    @greeting = "Hey there! I'm so excited to learn stuff."
-    @raise_hand = "Pick me!"
-  end
+  # def initialize
+  #   @name = name
+  #   @greeting = "Hey there! I'm so excited to learn stuff."
+  #   @raise_hand = "Pick me!"
+  # end
 
   def hello
     @greeting = "Hey there! I'm so excited to learn stuff."
@@ -13,6 +13,7 @@ class Student
   end
 
   def raise_hand
+    @raise_hand = "Pick me!"
     @raise_hand
   end
 
